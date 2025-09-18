04adc13ad945983ee79b4fb192ddec72668ae764
Farye Nwede <farye.nwede@hadrian.co>
2025-09-18 13:47:26 -0700

    chore(hadrian): Refactoring the structure to allow for different domains for my dotfiles.

--- hadrian/hadrian.sh
+++ hadrian/hadrian.sh
@@ -9,5 +9,14 @@ git-crypt init || warn "Git Crypt has already been initialized."
 # Grab the keys and add them to git-crypt
 for key in `gpg --list-keys --with-colons | awk -F':' '/^fpr:/ {print $10}'`; do git-crypt add-gpg-user $key; done
 
+# Install Terraform
+info "Installing Terraform"
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
+# Install the Silver Searcher
+info "Installling Silver Searcher"
+brew install the_silver_searcher
+
 info "Running the secrets BASH script"
-bash hadrian.secrets
+bash hadrian/hadrian.secrets
